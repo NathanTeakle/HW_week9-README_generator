a9abd665776f47edf0756fe3af63915ebe3a4ea9
NathanTeakle <59923059+NathanTeakle@users.noreply.github.com>
2020-04-29 18:33:40 +0930

    Update to index.js

--- index.js
+++ index.js
@@ -4,71 +4,69 @@ const fs = require("fs");
 //const utils = require("utils");
 
 // Inquirer prompts to guide file Generation.
-function userPrompt() {
-    return inquirer.prompt([{
-            type: "input",
-            name: "UsernameQuestion",
-            message: "Enter your Github username:",
-        },
-        {
-            type: "input",
-            name: "RepoUrl",
-            message: "Enter desired Github Repository URl:",
-        },
-        {
-            type: "input",
-            name: "ProjectTitle",
-            message: "Enter the Projects Title:",
-        },
-        {
-            type: "input",
-            name: "ProjectDescription",
-            message: "Enter a Description of the Project:",
-        },
-        {
-            type: "input",
-            name: "ProjectImage",
-            message: "Add the URL for your Image:",
-        },
-        {
-            type: "input",
-            name: "InstallationProcess",
-            message: "Add Installation steps:",
-        },
-        {
-            type: "input",
-            name: "ProjectUsage",
-            message: "Add Project Usage details:",
-        },
-        {
-            type: "list",
-            name: "UsedLicenses",
-            message: "Reference any Licenses used:",
-            choices: ["Apache License 2.0", "Mozilla Public License", "GNU Library", "none"]
-        },
-        {
-            type: "list",
-            name: "ProjectTesting",
-            message: "Reference Testing details:",
-            choices: ["npm", "none"]
-        },
-        {
-            type: "input",
-            name: "ProjectAuthor",
-            message: "Add name of Project Author:",
-        }, {
-            type: "input",
-            name: "ProjectAuthorEmail",
-            message: "Add email of Project Author:",
-        },
-    ]);
-}
+inquirer.prompt([{
+        type: "input",
+        name: "UsernameQuestion",
+        message: "Enter your Github username:",
+    },
+    {
+        type: "input",
+        name: "RepoUrl",
+        message: "Enter desired Github Repository URl:",
+    },
+    {
+        type: "input",
+        name: "ProjectTitle",
+        message: "Enter the Projects Title:",
+    },
+    {
+        type: "input",
+        name: "ProjectDescription",
+        message: "Enter a Description of the Project:",
+    },
+    {
+        type: "input",
+        name: "ProjectImage",
+        message: "Add the URL for your Image:",
+    },
+    {
+        type: "input",
+        name: "InstallationProcess",
+        message: "Add Installation steps:",
+    },
+    {
+        type: "input",
+        name: "ProjectUsage",
+        message: "Add Project Usage details:",
+    },
+    {
+        type: "list",
+        name: "UsedLicenses",
+        message: "Reference any Licenses used:",
+        choices: ["Apache License 2.0", "Mozilla Public License", "GNU Library", "none"]
+    },
+    {
+        type: "list",
+        name: "ProjectTesting",
+        message: "Reference Testing details:",
+        choices: ["npm", "none"]
+    },
+    {
+        type: "input",
+        name: "ProjectAuthor",
+        message: "Add name of Project Author:",
+    }, {
+        type: "input",
+        name: "ProjectAuthorEmail",
+        message: "Add email of Project Author:",
+    },
 
+    // Code to compile inputs into outputs as a README File.
 
-// Code to compile inputs into outputs as a README File.
-
-const listTable = `
+]).then(data => {
+    const listTable = `
         * UsernameQuestion
+        ${data.username}
         * RepoUrl
         * ProjectTitle
         * Project Description
@@ -80,14 +78,7 @@ const listTable = `
         * ProjectAuthor
         * ProjectAuthorEmail`;
 
-userPrompt()
-    .then(data => {
-        const listTable = `
-        ${data.username}
-        
-    `;
-
-        fs.writeFile("README_nt.md", listTable, function (err) {
-            return console.log(err);
-        });
+    fs.writeFile("README_nt.md", listTable, function (err) {
+        return console.log(err);
     });
+});
