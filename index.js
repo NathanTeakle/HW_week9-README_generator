c2c1984f54fe7f7e559bf3840fa2daabdd371180
NathanTeakle <59923059+NathanTeakle@users.noreply.github.com>
2020-04-29 18:15:53 +0930

    Update index.js

--- index.js
+++ index.js
@@ -8,61 +8,86 @@ function userPrompt() {
     return inquirer.prompt([{
             type: "input",
             name: "UsernameQuestion",
-            message: "Enter your Github username",
+            message: "Enter your Github username:",
         },
         {
             type: "input",
             name: "RepoUrl",
-            message: "Enter desired Github Repository URl",
+            message: "Enter desired Github Repository URl:",
         },
         {
             type: "input",
             name: "ProjectTitle",
-            message: "Enter Projects Title",
+            message: "Enter the Projects Title:",
         },
         {
             type: "input",
             name: "ProjectDescription",
-            message: "Enter a Description of the Project",
+            message: "Enter a Description of the Project:",
         },
         {
             type: "input",
             name: "ProjectImage",
-            message: "Add the URL for your Image",
+            message: "Add the URL for your Image:",
         },
         {
             type: "input",
             name: "InstallationProcess",
-            message: "Add Installation steps",
+            message: "Add Installation steps:",
         },
         {
             type: "input",
             name: "ProjectUsage",
-            message: "Add Project Usage details",
+            message: "Add Project Usage details:",
         },
         {
             type: "list",
             name: "UsedLicenses",
-            message: "Reference any Licenses used",
+            message: "Reference any Licenses used:",
             choices: ["Apache License 2.0", "Mozilla Public License", "GNU Library", "none"]
         },
         {
             type: "list",
             name: "ProjectTesting",
-            message: "Reference Testing details",
+            message: "Reference Testing details:",
             choices: ["npm", "none"]
         },
         {
             type: "input",
             name: "ProjectAuthor",
-            message: "Add name of Project Author",
+            message: "Add name of Project Author:",
         }, {
             type: "input",
             name: "ProjectAuthorEmail",
-            message: "Add email of Project Author",
+            message: "Add email of Project Author:",
         },
-    ])
+    ]);
 }
 
-// Code to compile results.
-// Yet to be done, I have no idea how to do it.
+
+// Code to compile inputs into outputs as a README File.
+
+const listTable = `
+        * UsernameQuestion
+        * RepoUrl
+        * ProjectTitle
+        * Project Description
+        * ProjectImage
+        * InstallationProcess
+        * ProjectUsage
+        * UsedLicenses
+        * ProjectTesting
+        * ProjectAuthor
+        * ProjectAuthorEmail`;
+
+userPrompt()
+    .then(data => {
+        const listTable = `
+        ${data.username}
+        
+    `;
+
+        fs.writeFile("README_nt.md", listTable, function (err) {
+            return console.log(err);
+        });
+    });
